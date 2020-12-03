b3b6f4e36d991fc8321944a822a95554e822330e
Tushar Sandhu <tusharsandhu3022@gmail.com>
2020-12-02 22:15:40 -0800

    baristo up

---
+++ baristo/assets/js/button.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  confirm("Thank You For Contacting :)");
+}
---
+++ baristo/assets/js/coupon.js
@@ -0,0 +1,14 @@
+
+
+function checkbox() {
+var checkBox = document.getElementById("mycheckbox");
+
+
+if (checkBox.checked  !=false){
+alert("YES YOU GET THE COUPON");
+return true;
+}
+else{
+	alert("YOU DID'NT GET THE COUPON ");
+  return false;
+}
